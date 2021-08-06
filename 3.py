55fb449c2f882508a8802e6abcac871cedb5ed1b
Ashutosh472 <68743933+Ashutosh472@users.noreply.github.com>
2021-08-06 16:18:49 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+n=int(input("Enter number of strings to be checked:"))
+ls=[]
+for i in range(0,n):
+  str1=input()
+  ls.append(str1)
+
+map1=list(map(lambda in_str:in_str.split(" ")[0], ls))
+print(map1)
